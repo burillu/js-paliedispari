b2e66e4716ddb77b6a90cb5853369c26b999e758
burillu <burillu90@hotmail.it>
2023-10-17 15:01:18 +0200

    add reverse for

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ btnPali.addEventListener ('click', isPalindormo)
 // controllare se la parola inserita è palindroma
 function isPalindormo(){
     //checkInput(wordInput, )
-    const inputValue= wordInput.value;
-    console.log(inputValue);
+    const inputValue= wordInput.value.toLowerCase();
+
+    let reverse ='';
+    for (let i = inputValue.length -1; i >= 0; i--) {
+        reverse += inputValue[i];
+        
+    }
+    console.log(reverse);
 }
 
 function checkInput (input, chk){
