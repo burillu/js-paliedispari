2928f0992f2be00fcedfff976f7d7e3c46528e5b
burillu <burillu90@hotmail.it>
2023-10-17 16:22:34 +0200

    on click get values from select and input

--- js/script.js
+++ js/script.js
@@ -25,3 +25,34 @@ function checkInput (input, chk){
 
 }
 
+
+// Pari e dispari
+
+// bottone pari e dispari
+const btnPariDispari= document.getElementById('btn-pari-dispari');
+btnPariDispari.addEventListener('click', playOddEven);
+
+// scegliere pari o dispari
+
+
+    
+// inserire un numero
+const numInput = document.getElementById('number');//(valore input)
+console.log(numInput);
+// genera un numero randomico da uno a 5 e sommalo al numero dell'utente
+
+//sommare i due numeri
+
+//dichiarare il vincitore
+
+
+
+function playOddEven(){
+    const pariDispari = document.querySelector('.pari-dispari');//(valore select)
+    console.log(pariDispari)
+    let oddeven = pariDispari.value;
+    console.log(oddeven);
+    let num = numInput.value
+    console.log(num);
+    
+}
