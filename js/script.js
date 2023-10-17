b094d0b33e756884c2f100ce956c2c5fe05ae8a1
burillu <burillu90@hotmail.it>
2023-10-17 14:40:23 +0200

    add function ispalindromo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 // Palindroma
 
 // inserire parola
-
+const wordInput= document.getElementById('inputPalindromo');
+const btnPali = document.getElementById('btn-pali');
+btnPali.addEventListener ('click', isPalindormo)
 // controllare se la parola inserita è palindroma
+function isPalindormo(){
+    //checkInput(wordInput, )
+    const inputValue= wordInput.value;
+    console.log(inputValue);
+}
+
+function checkInput (input, chk){
+
+}
 
